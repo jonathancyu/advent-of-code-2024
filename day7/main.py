857e9171c00ede6b1d063f67a992ddd981f39c7a
Jonathan Yu <yucjonathan@gmail.com>
2024-12-07 00:10:59 -0600

    feat: strategy pattern

--- day7/main.py
+++ day7/main.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from itertools import product
 from dataclasses import dataclass
 from collections import defaultdict, deque
-from typing import Counter, Optional
+from typing import Callable, Counter, Optional
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from tqdm.auto import tqdm
 
@@ -23,31 +23,15 @@ class Equation:
         return Equation(left=int(left), right=[int(x) for x in right.strip().split()])
 
 
-def sat(left: int, right: list[int]) -> bool:
+def sat(left: int, right: list[int], rules: list[Callable[[int, int], int]]) -> bool:
     if len(right) == 1:
         return left == right[0]
     current, next = right[:2]
     remaining = right[2:]
-    return (
-        sat(left, [current * next] + remaining)
-        or sat(left, [current + next] + remaining)
-        or sat(left, [int(f"{current}{next}")] + remaining)
-    )
-
-
-def part_one(equations: list[Equation]) -> int:
-    total = 0
-    for equation in equations:
-        target = equation.left
-        result = sat(equation.left, equation.right)
-        if result:
-            total += target
-
-    return total
-
-
-def part_two(equations: list[Equation]) -> int:
-    return 0
+    for rule in rules:
+        if sat(left, [rule(current, next)] + remaining, rules):
+            return True
+    return False
 
 
 if __name__ == "__main__":
@@ -58,6 +42,21 @@ if __name__ == "__main__":
         lines = f.readlines()
     equations: list[Equation] = [Equation.from_line(line) for line in lines]
 
-    print(f"Part one: {part_one(equations)}")
+    rules: list[Callable[[int, int], int]] = [
+        lambda x, y: x + y,
+        lambda x, y: x * y,
+        lambda x, y: int(f"{x}{y}"),
+    ]
+    part_one = sum(
+        equation.left
+        for equation in equations
+        if sat(equation.left, equation.right, rules[:2])
+    )
+    print(f"Part one: {part_one}")
 
-    # print(f"Part two: {part_two(visited, matrix, pos, direction)}")
+    part_two = sum(
+        equation.left
+        for equation in equations
+        if sat(equation.left, equation.right, rules)
+    )
+    print(f"Part two: {part_two}")
